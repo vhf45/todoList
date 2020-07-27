92e57d2fd260b6b534a0164f6756ad2eb248f9c2
td <vhf45@outlook.com>
2020-07-27 14:17:52 +0100

    env variables

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 
 let mongoose = require('mongoose');
 mongoose.set('debug', true);
-const url = 'mongodb://127.0.0.1:27017/chapter13apis';
+const url = "mongodb://testuser1:Qwerty22.@ds137863.mlab.com:37863/todolist";
 mongoose.connect(url, { useNewUrlParser: true });
 mongoose.Promise = Promise;
 
